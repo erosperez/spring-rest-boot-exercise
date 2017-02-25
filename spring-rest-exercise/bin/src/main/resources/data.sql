7e36a2783405a570c20b40eefc7e6c3469224c94
jperez <jperez@HP-PC>
2017-02-25 16:46:35 +0100

    Commit eliminación tras reestructuración project

--- spring-rest-exercise/bin/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into test.users(id, name, birthdate) values(1,'Eros','23/12/2000');
-insert into test.users(id, name, birthdate) values(2,'Juan','12/02/2000');
-insert into test.users(id, name, birthdate) values(3,'Jose','07/01/2011');
--- spring-rest-exercise/bin/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-CREATE schema if not exists test;  
-
-CREATE TABLE test.users
-(
-    id int(11) NOT NULL AUTO_INCREMENT,
-    name varchar(100) NOT NULL,
-    birthdate varchar(10) DEFAULT NULL,
-    PRIMARY KEY (id)
-);
